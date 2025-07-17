e2e86ecceb5ea5ded2324d9e785be8ffedd436f3
ViniJesus <vini.silva1342@gmail.com>
2025-07-17 16:59:52 -0300

    Sessoes até o sobre

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileButton = document.getElementById("mobile-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  mobileButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+  });
+  mobileMenu.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+    mobileMenu.classList.remove("flex");
+  });
+});
