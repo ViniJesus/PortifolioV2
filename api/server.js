bff9a615b086f18ef3df4cdf2a6fac1a37dca452
ViniJesus <vini.silva1342@gmail.com>
2025-07-24 10:40:41 -0300

    Ajustes para deploy

--- api/server.js
+++ api/server.js
@@ -1,46 +1,48 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static("."));
+// app.use(express.static("."));
 
 const projects = [
   {
-    name: "LifeSurf",
-    thumb: "src/img/projects/lifesurf.png",
-    description: "TESTE",
-    github: "https://github.com/ViniJesus/LifeSurf.git",
-    deploy: "https://life-surf.vercel.app/",
+    name: 'LifeSurf',
+    thumb: 'src/img/projects/lifesurf.png',
+    description: 'TESTE',
+    github: 'https://github.com/ViniJesus/LifeSurf.git',
+    deploy: 'https://life-surf.vercel.app/',
   },
   {
-    name: "Mindtechcare",
-    thumb: "src/img/projects/mindtechcare.png",
-    description: "TESTE",
-    github: "https://github.com/ViniJesus/MindTechCare",
-    deploy: "https://mindtechcare.onrender.com",
+    name: 'Mindtechcare',
+    thumb: 'src/img/projects/mindtechcare.png',
+    description: 'TESTE',
+    github: 'https://github.com/ViniJesus/MindTechCare',
+    deploy: 'https://mindtechcare.onrender.com',
   },
   {
-    name: "Forest",
-    thumb: "src/img/projects/forest.png",
-    description: "TESTE",
-    github: "https://github.com/ViniJesus/Forest",
-    deploy: "https://forest-snowy-psi.vercel.app/",
+    name: 'Forest',
+    thumb: 'src/img/projects/forest.png',
+    description: 'TESTE',
+    github: 'https://github.com/ViniJesus/Forest',
+    deploy: 'https://forest-snowy-psi.vercel.app/',
   },
   {
-    name: "PatyDoces",
-    thumb: "src/img/projects/patydoces.png",
-    description: "TESTE",
-    github: "https://github.com/ViniJesus/paty-doces",
-    deploy: "https://paty-doces.vercel.app/",
+    name: 'PatyDoces',
+    thumb: 'src/img/projects/patydoces.png',
+    description: 'TESTE',
+    github: 'https://github.com/ViniJesus/paty-doces',
+    deploy: 'https://paty-doces.vercel.app/',
   },
 ];
 
-app.get("/api/projects", (req, res) => {
+app.get('/api/projects', (req, res) => {
   res.json(projects);
 });
 
-app.listen(PORT, () => {
-  console.log(`API rodando em http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`API rodando em http://localhost:${PORT}`);
+// });
+
+module.exports = app;
